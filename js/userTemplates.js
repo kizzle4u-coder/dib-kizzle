e200944aa52ef74045922168b6109c4e5dbff7cc
kizzle4u-coder <kizzle4u@gmail.com>
2026-01-06 12:17:31 -0500

    Create userTemplates.js

---
+++ js/userTemplates.js
@@ -0,0 +1,11 @@
+export async function getTemplates() {
+  return [];
+}
+
+export async function saveTemplate(name, flags) {
+  console.log("Stub saveTemplate:", name, flags);
+}
+
+export async function deleteTemplate(id) {
+  console.log("Stub deleteTemplate:", id);
+}
